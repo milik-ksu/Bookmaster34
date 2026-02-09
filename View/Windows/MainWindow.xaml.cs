4518bce8bdcb76fa00d35c938c652202cca42850
vboxuser <ksugaberkorn07@mail.ru>
2026-02-09 13:28:18 +0300

    Добавьте файлы проекта.

---
+++ View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bookmaster34
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseAppMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogoutMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BrowsingTheCatalogMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
