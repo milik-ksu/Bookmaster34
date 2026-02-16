326a4bc76e6b1cb9a5da70f6a6fd7dd8aa4c87b7
vboxuser <ksugaberkorn07@mail.ru>
2026-02-16 13:39:39 +0300

    реализованна страничная навигация

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Bookmaster34.View.Pages;
+using Bookmaster34.View.Windows;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,7 +25,14 @@ namespace Bookmaster34
 
         private void LoginMi_Click(object sender, RoutedEventArgs e)
         {
+            LoginWindow loginWindow = new LoginWindow();
+            if (loginWindow.ShowDialog() == true)
+            {
+                LibraryMi.Visibility = Visibility.Visible;
+                LogoutMi.Visibility = Visibility.Visible;
+                LoginMi.Visibility = Visibility.Collapsed;
 
+            }
         }
 
         private void CloseAppMi_Click(object sender, RoutedEventArgs e)
@@ -33,27 +42,30 @@ namespace Bookmaster34
 
         private void LogoutMi_Click(object sender, RoutedEventArgs e)
         {
+                LibraryMi.Visibility = Visibility.Collapsed;
+                LogoutMi.Visibility = Visibility.Collapsed;
+                LoginMi.Visibility = Visibility.Visible;
 
         }
 
         private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new ManageCustomersPage());
         }
 
         private void CirculationMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new CirculationPage());
         }
 
         private void ReportsMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new ReportsPage());
         }
 
-        private void BrowsingTheCatalogMi_Click(object sender, RoutedEventArgs e)
+        private void BrowseCatalogMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.Navigate(new BrowseCatalogPage());
         }
     }
 }
